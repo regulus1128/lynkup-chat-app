ae2eaa049e903721e997e0a0fc9a9055239abc2e
Suraj Shyam <102654234+regulus1128@users.noreply.github.com>
2025-06-19 19:46:34 +0530

    fixes

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://lynkup-zeta.vercel.app",
     credentials: true,
 }))
 
