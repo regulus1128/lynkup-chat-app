3f41b27ddf145476ea2d2f1e4e26b5210f567256
Suraj Shyam <102654234+regulus1128@users.noreply.github.com>
2025-06-19 20:54:47 +0530

    fixed url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const URL = import.meta.env.VITE_BASE_URL;
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? `${URL}/api` : "",
+    baseURL: import.meta.env.MODE === "development" ? `${URL}/api` : `${URL}/api`,
     withCredentials: true
 });
 
